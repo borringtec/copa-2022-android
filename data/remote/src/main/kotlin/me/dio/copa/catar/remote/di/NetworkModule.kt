68479f429a4899d1cd97a640aab1782dd89c1589
borringtec <borringtec@gmail.com>
2024-11-20 16:57:18 -0300

    Change json API

--- data/remote/src/main/kotlin/me/dio/copa/catar/remote/di/NetworkModule.kt
+++ data/remote/src/main/kotlin/me/dio/copa/catar/remote/di/NetworkModule.kt
@@ -13,7 +13,7 @@ import javax.inject.Singleton
 
 @Module
 class NetworkModule {
-    private val url = "https://digitalinnovationone.github.io/copa-2022-android/"
+    private val url = "https://borringtec.github.io/copa-2022-android/"
 
     @Provides
     @Singleton
